177f60855dd54919fe43bbc6481113ef2f356af1
anneprt <anne.parrot82@gmail.com>
2018-09-24 19:52:26 +0200

    Réécriture de l'url en mapping
    ATTENTION : la default doit toujours être en dernier pour qu'elle soit appelée quand les autres ne peuvent pas l'être

--- Archery/App_Start/RouteConfig.cs
+++ Archery/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace Archery
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "AboutRoute",
+                url: "a-propos",
+                defaults: new { controller = "Home", action = "About" }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
