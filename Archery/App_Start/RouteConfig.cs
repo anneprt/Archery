5dbd21195014ccddf7f421c2406352d54cfd6efc
anneprt <anne.parrot82@gmail.com>
2018-09-24 12:49:52 +0200

    creation du projet

--- Archery/App_Start/RouteConfig.cs
+++ Archery/App_Start/RouteConfig.cs
@@ -18,6 +18,6 @@ namespace Archery
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-        }
+        } 
     }
 }
