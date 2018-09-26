be2df9bd9fa035488b67f7180941a0be3d87dfd1
anneprt <anne.parrot82@gmail.com>
2018-09-26 12:53:51 +0200

    ajout du parametre archer de type Archer pour recuperer les donnees du formulaire

--- Archery/Controllers/ArchersController.cs
+++ Archery/Controllers/ArchersController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Archery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,7 +17,7 @@ namespace Archery.Controllers
         }
 
         [HttpPost]
-        public ActionResult Subscribe(string email)
+        public ActionResult Subscribe(Archer archer)
         {
             return View();
         }
