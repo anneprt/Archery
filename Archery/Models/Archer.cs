1299d5b25575a98f0c7448fe998a1bc1a78af943
anneprt <anne.parrot82@gmail.com>
2018-09-26 10:48:31 +0200

    renommage du model player en archer

---
+++ Archery/Models/Archer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Archery.Models
+{
+    public class Archer : User
+    {
+        public string LicenceNumber { get; set; }
+
+    }
+}
