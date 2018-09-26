9030ab8ed5df688d29bb7eb3c3933a3d1c98bc3b
anneprt <anne.parrot82@gmail.com>
2018-09-26 10:47:51 +0200

    creation de ArchersController (modification nom)

---
+++ Archery/Controllers/ArchersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Archery.Controllers
+{
+    public class ArchersController : Controller
+    {
+        // GET: Players
+        public ActionResult Subscribe()
+        {
+            return View();
+        }
+
+
+    }
+}
