ff5f7ae5b1a64c822bf24c25296eac54d5f3eb81
anneprt <anne.parrot82@gmail.com>
2018-09-28 15:28:35 +0200

    securité AntiForgeryToken et attribut Bind dans le controller

--- Archery/Controllers/ArchersController.cs
+++ Archery/Controllers/ArchersController.cs
@@ -21,7 +21,8 @@ namespace Archery.Controllers
         }
 
         [HttpPost]
-        public ActionResult Subscribe(Archer archer)
+        [ValidateAntiForgeryToken]
+        public ActionResult Subscribe([Bind(Exclude ="ID")]Archer archer)
         {
             if(DateTime.Now.AddYears(-9)<= archer.BirthDate)
             {
