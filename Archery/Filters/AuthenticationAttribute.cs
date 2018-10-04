8c8d3cb2ae41cb500600deb669ed63d0b02958c3
anneprt <anne.parrot82@gmail.com>
2018-10-05 00:15:36 +0200

    creation du dossier Filters, creation classe AuthenticationAttribute (identity framework)==filtre d'authenfication

---
+++ Archery/Filters/AuthenticationAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Archery.Filters
+{
+    public class AuthenticationAttribute : ActionFilterAttribute
+
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            filterContext.Result = new RedirectResult(@"\backoffice\authentication\login");
+            //filterContext.Result = new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary(new{ controller="authentication", action= "login", area="backoffice" }));
+        }
+    }
+}
