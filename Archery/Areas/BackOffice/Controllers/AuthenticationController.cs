5cc5862fa0f55294a5ead44580b57e167a7a4a93
anneprt <anne.parrot82@gmail.com>
2018-10-04 23:48:14 +0200

    creation Authentification controller avec action Login

---
+++ Archery/Areas/BackOffice/Controllers/AuthenticationController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Archery.Areas.BackOffice.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        // GET: BackOffice/Authentication
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(AuthenticationLoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var hash = model.Password.HashMD5();
+                var admin = db.Administrators.SingleOrDefault(
+                    x => x.Mail == model.Mail && x.Password == hash);
+                if (admin == null)
+                {
+                    ModelState.AddModelError("Mail", "Login / mot de passe invalide");
+                    return View();
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Dashboard", new { area = "backoffice" });
+                }
+            }
+            return View();
+        }
+    }
+}
+}
