5d3d958a98d0d4c82bbb52eb6e20ac5ec598a01e
anneprt <anne.parrot82@gmail.com>
2018-10-06 00:51:38 +0200

    Ajout authentification Archer

--- Archery/Filters/AuthenticationAttribute.cs
+++ Archery/Filters/AuthenticationAttribute.cs
@@ -6,13 +6,40 @@ using System.Web.Mvc;
 
 namespace Archery.Filters
 {
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
     public class AuthenticationAttribute : ActionFilterAttribute
 
     {
+        public string Type { get; set; } = "BO";
+
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             filterContext.Result = new RedirectResult(@"\backoffice\authentication\login");
             //filterContext.Result = new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary(new{ controller="authentication", action= "login", area="backoffice" }));
+
+
+            if (Type == "ARCHER")
+            {
+
+                if (filterContext.HttpContext.Session["ARCHER"] == null)
+                {
+                    filterContext.Controller.TempData["REDIRECT"] = filterContext.HttpContext.Request.Url.AbsoluteUri;
+                    filterContext.Result = new RedirectResult(@"\archers\login");
+                }
+            }
+            if (Type == "BO")
+            {
+                if (filterContext.HttpContext.Session["ADMINISTRATOR"] == null)
+                {
+                    filterContext.Result = new RedirectResult(@"\backoffice\authentication\login");
+                    //filterContext.Result = new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary(new{ controller="authentication", action= "login", area="backoffice" }));
+                }
+
+
+
+            }
+
+
+
         }
     }
-}
