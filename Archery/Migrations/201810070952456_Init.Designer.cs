bc47cca1def33bb7290d938734e40a932269276a
anneprt <anne.parrot82@gmail.com>
2018-10-07 22:38:38 +0200

    Drop de la base existante pour correction (oubli de migration suite chgt models)

--- Archery/Migrations/201810070952456_Init.Designer.cs
+++ Archery/Migrations/201810070952456_Init.Designer.cs
@@ -7,13 +7,13 @@ namespace Archery.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class TournamentPictures : IMigrationMetadata
+    public sealed partial class Init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(TournamentPictures));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Init));
         
         string IMigrationMetadata.Id
         {
-            get { return "201810042257324_TournamentPictures"; }
+            get { return "201810070952456_Init"; }
         }
         
         string IMigrationMetadata.Source
--- Archery/Migrations/Configuration.cs
+++ Archery/Migrations/Configuration.cs
@@ -18,6 +18,7 @@ namespace Archery.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+            context.Archers.Add
         }
     }
 }
